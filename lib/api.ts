8cf42c34d6e7fdc58dba587e49196bd17efac4a2
Waseem-Baig <waseem20032005@gmail.com>
2025-09-12 16:32:48 +0530

    ALL@

--- lib/api.ts
+++ lib/api.ts
@@ -7,7 +7,8 @@ class ApiClient {
 
   constructor() {
     this.client = axios.create({
-      baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api",
+      baseURL:
+        `${process.env.NEXT_PUBLIC_API_URL}/api` || "http://localhost:3001/api",
       timeout: 30000,
       headers: {
         "Content-Type": "application/json",
